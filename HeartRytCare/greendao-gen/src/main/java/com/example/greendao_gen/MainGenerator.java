2ac4535953a3acfeb09357d21c944d85ce527566
patledesma <patdledesma@gmail.com>
2018-01-08 01:08:44 +0800

    added greendao

---
+++ HeartRytCare/greendao-gen/src/main/java/com/example/greendao_gen/MainGenerator.java
@@ -0,0 +1,41 @@
+package com.example.greendao_gen;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+public class MainGenerator {
+
+    private static final String PROJECT_DIR = System.getProperty("user.dir");
+
+    public static void main(String[] args) {
+        Schema schema = new Schema(1, "com.example.hp.heartrytcare.db");
+        schema.enableKeepSectionsByDefault();
+
+        addTables(schema);
+
+        try {
+            new DaoGenerator().generateAll(schema, PROJECT_DIR + "\\app\\src\\main\\java");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addTables(final Schema schema) {
+        Entity user = addUser(schema);
+    }
+
+    private static Entity addUser(final Schema schema) {
+        Entity user = schema.addEntity("User");
+        user.addIdProperty().primaryKey().autoincrement();
+        user.addIntProperty("user_id").notNull();
+        user.addIntProperty("user_type").notNull(); // 1 = doctor | 0 = patient
+        user.addStringProperty("last_name").notNull();
+        user.addStringProperty("first_name").notNull();
+        user.addStringProperty("license_number");
+        user.addStringProperty("email");
+        user.addStringProperty("contact_number");
+
+        return user;
+    }
+}
